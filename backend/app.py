90169c2cba334d83c9b175ca7a63e9cf998866a4
Yuan <wang17@ualberta.ca>
2020-10-18 11:47:37 +0800

    Add data from DHT22 to the backend response

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,23 @@
 from flask import Flask, Response
-from flask_cors import CORS
+import Adafruit_DHT
 import json
 
 app = Flask(__name__)
-CORS(app)
 
+# Adafruit_DHT.DHT22 sensor
+sensor = Adafruit_DHT.DHT22
+# connected to GPIO4.
+pin = 4
 
-@app.route('/api/gettmp',methods=["GET", "POST"])
+@app.route('/api/gettmp',methods=["GET"])
 def draw_stone():
-    d_hum,d_temp = 34.56,20.8
-    d_hum = round(d_hum,2)
-    d_temp = round(d_temp,2)
-    data = [d_temp,d_hum]
-    return Response(json.dumps(data),  mimetype='application/json')
-
-@app.route('/')
-def staff_page():
-    return app.send_static_file('index.html')
+    humidity,temperature = Adafruit_DHT.read_retry(sensor, pin)
+    humidity = round(humidity,2)
+    temperature = round(temperature,2)
+    data = [temperature,humidity]
+    resp = Response(json.dumps(data),  mimetype='application/json')
+    resp.headers['Access-Control-Allow-Origin'] = 'http://raspberrypi.design'
+    return resp
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000)
