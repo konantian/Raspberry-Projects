9d245ee162ae03edeafa6385a7a1706d74826d05
Yuan <wang17@ualberta.ca>
2020-10-29 20:06:41 +0800

    Add camera component

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, Response
+from flask import Flask, Response, send_file, jsonify
 import Adafruit_DHT
+import base64
+from picamera import PiCamera
 from flask_cors import CORS
 from time import sleep
 import json
@@ -12,6 +14,9 @@ sensor = Adafruit_DHT.DHT22
 # connected to GPIO4.
 pin = 4
 
+# camera
+camera = PiCamera()
+
 @app.route('/api/gettmp',methods=["GET"])
 def get_temp_humi():
     humidity,temperature = Adafruit_DHT.read_retry(sensor, pin)
@@ -23,6 +28,18 @@ def get_temp_humi():
     data = [temperature,humidity]
     resp = Response(json.dumps(data),  mimetype='application/json')
     return resp
+
+@app.route('/api/capture, methods=["GET"])
+def get_capture():
+    camera.start_preview()
+    sleep(5)
+    camera.capture('./image.jpg')
+    camera.stop_preview()
+    filename = "./image.jpg"
+    with open(filename, 'rb') as f:
+        im_b64 = base64.b64encode(f.read())
+    return jsonify({'image' : im_b64})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000)
